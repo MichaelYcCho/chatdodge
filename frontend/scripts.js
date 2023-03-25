24c9059b7b5169462347ae99799637466a30b752
Michael Youngchan CHO <michael.yc.cho@gmail.com>
2023-03-25 19:49:17 +0900

    feat: Loading Spin

--- frontend/scripts.js
+++ frontend/scripts.js
@@ -1,58 +1,63 @@
 const chatBox = document.querySelector('.chat-box');
-let userMessages = [];
-let assistantMessages = [];
-let myDateTime = ''
+        let userMessages = [];
+        let assistantMessages = [];
+        let myDateTime = ''
 
-function start() {
-    const date = document.getElementById('date').value;
-    const hour = document.getElementById('hour').value;
-    if (date === '') {
-        alert('생년월일을 입력해주세요.');
-        return;
-    }
-    myDateTime = date + hour;
+        function spinner() {
+            document.getElementById('loader').style.display = "block";
+        }
 
-    document.getElementById("intro").style.display = "none";
-    document.getElementById("chat").style.display = "block";
-}
+        function start() {
+            const date = document.getElementById('date').value;
+            const hour = document.getElementById('hour').value;
+            if (date === '') {
+                alert('생년월일을 입력해주세요.');
+                return;
+            }
+            myDateTime = date + hour;
 
-const sendMessage = async () => {
-    const chatInput = document.querySelector('.chat-input input');
-    const chatMessage = document.createElement('div');
-    chatMessage.classList.add('chat-message');
-    chatMessage.innerHTML = `
-<p>${chatInput.value}</p>
-`;
-    chatBox.appendChild(chatMessage);
-    
-    //userMessage 메세지 추가
-    userMessages.push(chatInput.value);
+            document.getElementById("intro").style.display = "none";
+            document.getElementById("chat").style.display = "block";
+        }
 
-    chatInput.value = '';
+        const sendMessage = async () => {
+            const chatInput = document.querySelector('.chat-input input');
+            const chatMessage = document.createElement('div');
+            chatMessage.classList.add('chat-message');
+            chatMessage.innerHTML = `
+    <p>${chatInput.value}</p>
+  `;
+            chatBox.appendChild(chatMessage);
+            
+            //userMessage 메세지 추가
+            userMessages.push(chatInput.value);
 
-    const response = await fetch('http://localhost:3000/fortune-tell', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({
-            myDateTime: myDateTime,
-            userMessages: userMessages,
-            assistantMessages: assistantMessages,
-        })
-    });
+            chatInput.value = '';
 
-    const data = await response.json();
-    
-    //assistantMessage 메세지 추가
-    assistantMessages.push(data.assistant);
+            const response = await fetch('http://localhost:3000/fortune-tell', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+                    myDateTime: myDateTime,
+                    userMessages: userMessages,
+                    assistantMessages: assistantMessages,
+                })
+            });
 
-    const astrologerMessage = document.createElement('div');
-    astrologerMessage.classList.add('chat-message');
-    astrologerMessage.innerHTML = `
-<p class='assistant'>${data.assistant}</p>
-`;
-    chatBox.appendChild(astrologerMessage);
-};
+            const data = await response.json();
+            document.getElementById('loader').style.display = "none";
+            
+            //assistantMessage 메세지 추가
+            assistantMessages.push(data.assistant);
 
-document.querySelector('.chat-input button').addEventListener('click', sendMessage);
+            const astrologerMessage = document.createElement('div');
+            astrologerMessage.classList.add('chat-message');
+            astrologerMessage.innerHTML = `
+    <p class='assistant'>${data.assistant}</p>
+  `;
+            chatBox.appendChild(astrologerMessage);
+        };
+
+        document.querySelector('.chat-input button').addEventListener('click', sendMessage);
